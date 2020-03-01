b37adf9c83886277f6f32d6ae1f64aff1a7e916f
fred-vogt <fred.vogt@gmail.com>
2020-02-29 19:43:27 -0800

    Updated custom css

--- jenkins/server-image/init-scripts/configureSimpleThemePlugin.groovy
+++ jenkins/server-image/init-scripts/configureSimpleThemePlugin.groovy
@@ -15,21 +15,27 @@ desc.elements.add(cssNeoLight)
 
 def cssOverrides = new CssTextThemeElement('''\
 #header {
-    background-color: #0087f6;
+    background-color: #000000;
+    border-bottom: #ef7150 3px solid;
+    box-sizing: border-box;
+    height: 50px;
 }
 
-#breadcrumbBar, #footer-container, .top-sticker-inner {
-    background-color: #f1f1f1 !important;
+#header .searchbox {
+    padding: 5px 10px 0px 10px;
 }
 
+.top-sticker .noedge {
+    top: 56px;
+}
 .pane-frame .pane-header {
-    border-bottom: 2px solid #3477b4;
+    border-bottom: 2px solid #ef7150;
 }
 
 #header #search-box {
     color: #26262d;
-    padding-left: 20px;
-    background: white url(../images/16x16/search.png) no-repeat 2px center;
+    padding-left: 32px;
+    background: white;
 }
 ''')
 desc.elements.add(cssOverrides)
