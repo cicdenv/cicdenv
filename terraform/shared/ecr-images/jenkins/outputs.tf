4a9afd91910c12f32158e1a83d4d24f25ce45391
fred-vogt <fred.vogt@gmail.com>
2020-03-05 12:14:53 -0800

    Rollout Jenkins-2.223

--- terraform/shared/ecr-images/jenkins/outputs.tf
+++ terraform/shared/ecr-images/jenkins/outputs.tf
@@ -5,7 +5,7 @@ output "jenkins_server_image_repo" {
     arn            = aws_ecr_repository.jenkins_server.arn
     registry_id    = aws_ecr_repository.jenkins_server.registry_id
     repository_url = aws_ecr_repository.jenkins_server.repository_url
-    latest         = "2.222-2020.02.23-03"
+    latest         = "2.223-2020.03.01-01"
   }
 }
 
@@ -16,7 +16,7 @@ output "jenkins_agent_image_repo" {
     arn            = aws_ecr_repository.jenkins_agent.arn
     registry_id    = aws_ecr_repository.jenkins_agent.registry_id
     repository_url = aws_ecr_repository.jenkins_agent.repository_url
-    latest         = "2.222-2020.02.23-03"
+    latest         = "2.223-2020.03.01-01"
   }
 }
 
