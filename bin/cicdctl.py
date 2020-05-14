629a58c51037a8f74ab7549419ed13c45193d708
fred-vogt <fred.vogt@gmail.com>
2020-05-14 12:26:51 -0700

    show 'cicdctl' in Usage: output

--- bin/cicdctl.py
+++ bin/cicdctl.py
@@ -3,9 +3,13 @@
 import sys
 sys.path.append('lib')
 
+from os.path import basename, splitext
+
 from cicdctl import cli
 from cicdctl.commands import add_commands
 
+usage_name = splitext(basename(__file__))[0]
+
 if __name__ == "__main__":
     add_commands(cli)
-    cli()
+    cli(prog_name=usage_name)
