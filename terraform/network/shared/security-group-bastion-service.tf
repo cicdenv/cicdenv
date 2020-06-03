e3185385f84bdbc6cbbf5e64bdd2a3c1046087d7
fred-vogt <fred.vogt@gmail.com>
2020-06-03 07:28:45 -0700

    bastion tcp nlb health checks sg rules

--- terraform/network/shared/security-group-bastion-service.tf
+++ terraform/network/shared/security-group-bastion-service.tf
@@ -33,6 +33,18 @@ resource "aws_security_group_rule" "bastion_service_in" {
   cidr_blocks       = var.whitelisted_cidr_blocks
 }
 
+resource "aws_security_group_rule" "bastion_service_healthcheck" {
+  description = "bastion service ssh access from NLB"
+
+  type      = "ingress"
+  protocol  = "tcp"
+  from_port = var.ssh_service_port
+  to_port   = var.ssh_service_port
+
+  security_group_id = aws_security_group.bastion.id
+  cidr_blocks       = [module.shared_vpc.vpc.cidr_block]
+}
+
 resource "aws_security_group_rule" "bastion_host_in" {
   description = "bastion host ssh access from outside"
 
@@ -43,6 +55,18 @@ resource "aws_security_group_rule" "bastion_host_in" {
 
   security_group_id = aws_security_group.bastion.id
   cidr_blocks       = var.whitelisted_cidr_blocks
+}
+
+resource "aws_security_group_rule" "bastion_host_healthcheck" {
+  description = "bastion host ssh access from NLB"
+
+  type      = "ingress"
+  protocol  = "tcp"
+  from_port = var.ssh_host_port
+  to_port   = var.ssh_host_port
+
+  security_group_id = aws_security_group.bastion.id
+  cidr_blocks       = [module.shared_vpc.vpc.cidr_block]
 }
 
 resource "aws_security_group_rule" "events_host_in" {
