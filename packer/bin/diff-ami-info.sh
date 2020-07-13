72ec2aef7d4fe1b54b7b9c8a5c0f651094e5d43e
fred-vogt <fred.vogt@gmail.com>
2020-07-13 11:11:52 -0700

    packer - ami info diff script

---
+++ packer/bin/diff-ami-info.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+_PWD="$(pwd)"
+
+# Set working directory to project top-level folder
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+pushd "$DIR/../.." >/dev/null
+
+function usage () {
+    cat <<EOF >&2
+usage: $0 [packer/ami-info/<file1>] [packer/ami-info/<file2>]
+EOF
+}
+
+if [[ "$#" -gt 2 ]]; then  # Only [0-2] files to diff is supported
+    usage
+    exit 1
+fi
+
+_infos=("$@")  # Arguments if any
+# Look up the last 1 or 2 info files by age if less than 2 arguments
+if [[ "$#" -lt 2 ]]; then
+    IFS=$'\n' read -r -d '' -a _latest \
+        < <(find packer/ami-info/ -type f -printf '%T+ %p\n' \
+            | sort | tail -n $((2 - ${#_infos[@]})) | awk '{print $2}') || true
+fi
+diff_cmd=$(echo diff ${_infos[*]-} ${_latest[*]-})
+echo $diff_cmd
+
+# Output wihtout line numbers and groups removed
+$diff_cmd | grep -v '\-\-\-' | sed -e 's/^[^<>].*//'
