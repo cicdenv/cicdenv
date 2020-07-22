df3b18e864e17279d7b729409577a68ee021a31f
fred-vogt <fred.vogt@gmail.com>
2020-07-22 11:02:31 -0700

    jenkins - plugin version diff script

--- jenkins/plugin-versions/diff-versions.sh
+++ jenkins/plugin-versions/diff-versions.sh
@@ -10,7 +10,7 @@ pushd "$DIR/../.." >/dev/null
 
 function usage () {
     cat <<EOF >&2
-usage: $0 [packer/ami-info/<file1>] [packer/ami-info/<file2>]
+usage: $0 [jenkins/plugin-versions/<file1>] [jenkins/plugin-versions/<file2>]
 EOF
 }
 
@@ -19,16 +19,18 @@ if [[ "$#" -gt 2 ]]; then  # Only [0-2] files to diff is supported
     exit 1
 fi
 
-_infos=("$@")  # Arguments if any
+_versions=("$@")  # Arguments if any
 # Look up the last 1 or 2 info files by age if less than 2 arguments
 if [[ "$#" -lt 2 ]]; then
     IFS=$'\n' read -r -d '' -a _latest \
-        < <(find packer/ami-info/ -type f -name '*.txt' -printf '%T+ %p\n' \
-            | sort | tail -n $((2 - ${#_infos[@]})) | awk '{print $2}') || true
+        < <(find jenkins/plugin-versions/ -type f -name '*.txt' -printf '%T+ %p\n' \
+            | sort | tail -n $((2 - ${#_versions[@]})) | awk '{print $2}') || true
 fi
-diff_cmd=$(echo diff ${_infos[*]-} ${_latest[*]-})
-diff_count=$(($($diff_cmd | grep -v '\-\-\-' | sed -e 's/^[^<>].*//' | sed -r '/^$/d' | wc -l) / 2))
+
+diff_cmd=$(echo diff ${_versions[*]-} ${_latest[*]-})
 echo $diff_cmd
+
+diff_count=$(($($diff_cmd | grep -v '\-\-\-' | sed -e 's/^[^<>].*//' | sed -r '/^$/d' | wc -l) / 2)) || true
 echo "diff count: ${diff_count}"
 
 # Output wihtout line numbers and groups removed
