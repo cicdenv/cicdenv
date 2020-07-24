fee2ddf457c6c4e14c6b4e1b3ecd11767eb3433f
fred-vogt <fred.vogt@gmail.com>
2020-07-24 14:15:00 -0700

    jenkins - remove dist instance

--- terraform/jenkins/instances/dist/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  ami_id = var.base_ami_id != "" ? var.base_ami_id : data.terraform_remote_state.amis.outputs.base_ami.id
-}
--- terraform/jenkins/instances/dist/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 0.12.28"
-  backend "s3" {
-    key = "jenkins_instances_dist/terraform.tfstate"
-
-    workspace_key_prefix = "state"
-  }
-}
